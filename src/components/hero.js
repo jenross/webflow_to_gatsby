67acee5e53c3cbb707d6ea147aebf3dab13f0853
jenross <ross.m.jennifer@gmail.com>
2020-09-08 16:10:52 -0400

    hero section added

---
+++ src/components/hero.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Hero = () => {
+  return (
+    <>
+      <div class="section hero-section">
+        <div class="container">
+          <div class="hero-left">
+            <div class="horizontal-bar"></div>
+            <h1 class="hero-heading">
+              Hi, my name is Jen, and I&#x27;m a web developer in Orlando.
+            </h1>
+          </div>
+          <div class="hero-right">
+            <div class="hero-image"></div>
+            <ul role="list" class="list w-list-unstyled">
+              <li>Illustration</li>
+              <li>Branding</li>
+              <li>Photography</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Hero
